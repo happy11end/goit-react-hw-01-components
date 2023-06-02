21c3036db91354ce262bb47060f122b0957c6d37
Роман <114182599+happy11end@users.noreply.github.com>
2023-06-02 12:18:16 +0300

    profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
+import { Profile } from './profile/Profile';
+import user from './profile/user.json';
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
+       style={{
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        padding: 20,
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };

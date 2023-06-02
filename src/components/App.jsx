e77fe24845bbc63d50f276b5c8e272a0755c7f96
Роман <114182599+happy11end@users.noreply.github.com>
2023-06-02 16:43:45 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,12 @@
 import { Profile } from './profile/Profile';
 import user from './profile/user.json';
+import { Statistics } from './statistics/Statistics';
+import data from './statistics/data.json';
+import { FriendList } from './friendList/FriendList';
+import friends from './friendList/friends.json';
+import { TransactionHistory } from './transactionHistory/TransactionHistory';
+import transactions from './transactionHistory/transactions.json';
+
 export const App = () => {
   return (
     <div
@@ -19,6 +26,12 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+
+      <Statistics title="Upload stats" stats={data} />
+      
+      <FriendList friends={friends} />
+      
+       <TransactionHistory transactions={transactions} />
     </div>
   );
 };
---
+++ src/components/friendList/FriendList.jsx
@@ -0,0 +1,32 @@
+import styles from './FriendList.module.css';
+import PropTypes from 'prop-types';
+
+export const FriendList = ({friends}) => {
+    return (
+        <ul className={styles.list}>
+            {friends.map(FriendListItem)}
+        </ul>
+    );
+}
+
+
+function FriendListItem({ avatar, name, isOnline, id })  {
+    return (
+        <li key={id} className={styles.item}>
+            <span className={isOnline ? `${styles.status} ${styles.active}` : styles.status}></span>
+        <img className={styles.avatar} src={avatar} alt="User avatar " width="48" />
+            <p className={styles.name}>{name}</p>
+        </li>
+    )
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      isOnline: PropTypes.bool,
+      avatar: PropTypes.string,
+      name: PropTypes.string,
+    })
+  ),
+};

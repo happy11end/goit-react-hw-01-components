8003be96a35323231fb5a7a4049ff145598be6d6
Роман <114182599+happy11end@users.noreply.github.com>
2023-06-02 22:37:13 +0300

    1

--- src/components/friendList/FriendList.jsx
+++ src/components/friendList/FriendList.jsx
@@ -28,8 +28,8 @@ FriendList.propTypes = {
     name: PropTypes.string.isRequired,
     avatar: PropTypes.string.isRequired,
     isOnline: PropTypes.bool.isRequired,
-  })
-  )
+  }).isRequired
+  ).isRequired,
 };
 
 
